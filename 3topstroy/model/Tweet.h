54447c546d652f69cb6df583443586720bd9f9fc
MacBookPro <macbookpro@MacBookProui-MacBook-Pro.local>
2018-11-21 16:57:27 +0900

    Tweet 모델 객체 생성

--- 3topstroy/model/Tweet.h
+++ 3topstroy/model/Tweet.h
@@ -7,7 +7,16 @@
 //
 
 #import <Foundation/Foundation.h>
-
+#import <UIKit/UIKit.h>
 @interface Tweet : NSObject
+@property(nonatomic,strong) NSString *identifier;
+@property(nonatomic,strong) NSString *fromUser;
+@property(nonatomic,strong) NSString *fromUserDisplay;
+@property(nonatomic,strong) NSString *profileImageURL;
+@property(nonatomic,strong) NSString *text;
+@property(nonatomic,strong) NSDate   *createdDate;
+@property(nonatomic,strong) UIImage  *profileImage;
+
+- (id)initWithDictionary:(NSDictionary*)tweetData;
 
 @end
