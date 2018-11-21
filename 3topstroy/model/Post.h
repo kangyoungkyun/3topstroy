63f54ea4b91f49c059a1bdfbeae1b87b2b6a2a87
MacBookPro <macbookpro@MacBookProui-MacBook-Pro.local>
2018-11-21 16:30:09 +0900

    //post 객체 정의

--- 3topstroy/model/Post.h
+++ 3topstroy/model/Post.h
@@ -6,8 +6,23 @@
 //  Copyright © 2018 MacBookPro. All rights reserved.
 //
 
+//post 객체 정의
 #import <Foundation/Foundation.h>
 
 @interface Post : NSObject
+@property(nonatomic,strong) NSString        *title;             //rss
+@property(nonatomic,strong) NSString        *postDescription;   //rss
+@property(nonatomic,strong) NSString        *content;           //html
+@property(nonatomic,strong) NSString        *author;            //html
+@property(nonatomic,strong) NSString        *section;           //html
+@property(nonatomic,strong) NSString        *contentURL;        //rss
+@property(nonatomic,strong) NSDate          *pubDate;           //rss
+@property(nonatomic,strong) NSMutableArray  *keywords;          //html
+
+@property(nonatomic,strong) NSMutableArray  *tweets;
+@property(nonatomic,assign) BOOL            contentFetched;
+@property(nonatomic,assign) BOOL            tweetsLoading;
+
+- (NSDictionary*)dictionaryRepresentation;
 
 @end
--- 3topstroy/model/Post.m
+++ 3topstroy/model/Post.m
@@ -7,7 +7,25 @@
 //
 
 #import "Post.h"
-
+#import "Utils.h"
 @implementation Post
+
+@synthesize title, postDescription, content, author, section, contentURL, pubDate, keywords, tweets, contentFetched, tweetsLoading;
+
+
+- (NSDictionary*)dictionaryRepresentation {
+    NSLog(@"Post dictionaryRepresentation 호출");
+    NSString *pubDateString =
+    [NSString stringWithFormat:@"%@", self.pubDate];
+    
+    // content, tweets, and keywords were left off to limit size
+    return [NSDictionary dictionaryWithObjectsAndKeys:
+            [Utils urlEncode:self.title], @"title",
+            [Utils urlEncode:self.postDescription], @"description",
+            [Utils urlEncode:self.author], @"author",
+            [Utils urlEncode:self.section], @"section",
+            [Utils urlEncode:self.contentURL], @"contentURL",
+            [Utils urlEncode:pubDateString], @"pubDate", nil];
+}
 
 @end
